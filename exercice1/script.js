4827561040e5d75e8d423be194df4189947af309
Lyne Kapongo Tshilumbwa <Lynekpng@gmail.com>
2024-05-10 12:21:42 +0200

    first commit

---
+++ exercice1/script.js
@@ -0,0 +1,17 @@
+document.getElementById("button").addEventListener("click", function() {
+    fetch("exercice1/localstorage.json")
+        .then(response => response.json())
+        .then (data => {
+            const rulesList = document.getElementById("rulesList");
+            rulesList.innerHTML = "";
+            data.forEach(rule => {
+                const ListItem = document.createElement("li") ;
+                ListItem.textContent = rule ;
+                rulesList.appendChild(ListItem);
+                
+            });
+        })
+        .catch(error => {console.error('Error fetching JSON: ', error);
+
+    });
+});
